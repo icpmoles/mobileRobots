b6bef5a33b497bb5f369aada360840effaf22196
Iacopo Moles <iacopo@icpmol.es>
2024-11-13 17:34:07 +0100

    create test node
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>

---
+++ src/yearwork/turtlebot_simulator/src/turtlebot_cmd_input.cpp
@@ -0,0 +1,53 @@
+#include "ros/ros.h"
+// #include "std_msgs/String.h"
+// #include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Twist.h"
+
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "tester"); // intialize the node with a meaningful name
+	
+	//, ros::init_options::AnonymousName); // intialize the node with an "automatic random generated" name without needing to remap at every start in case of conflict
+	ros::NodeHandle n; //create nodeHandle
+
+	ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("tb_cmd", 1);
+	std::string node_name = ros::this_node::getName().c_str();
+	float v,omega,period;
+	// Handle.getParam(run_period_name, RunPeriod);
+
+	n.getParam(node_name+"/v",v);
+	n.getParam(node_name+"/omega",omega);
+	n.getParam(node_name+"/period",period);
+
+	//create publisher object with node.advertise with a type string and name "chatter" and 1 as size of the buffer of the publisher (1 is good most of the time)(can be increased in case your calculations take too much time)
+
+	ros::Rate loop_rate(1.0/period);
+	// running frequency of the loop at 10Hz
+
+	int count = 0;
+
+  	while (ros::ok()){ //standard ros loop, check if ROS is working, exit otherwise
+
+	    	geometry_msgs::Twist msg; 
+
+                
+			msg.linear.x = v; //ros msg object only has data field
+			msg.angular.z = omega;
+    		ROS_INFO("%s: Publishing test vel_cmd for turtlebot",node_name); //standard ROS logging/debugging message
+			// we just log the content of our msg
+
+    		chatter_pub.publish(msg);
+			// we just publish the content of our msg
+
+    		ros::spinOnce();
+			// handles all the other generic ros tasks like callbacks timers etc, useful for more complex
+			// code
+
+    		loop_rate.sleep(); //just waits until the next 10Hz loop, better this way 
+			// instead of the built in sleep
+			
+  	}
+
+
+  	return 0;
+}
