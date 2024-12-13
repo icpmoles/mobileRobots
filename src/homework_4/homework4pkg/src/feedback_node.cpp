7b2a6e15a291c8f2eac77e55a2c2923ee6c01230
Iacopo Moles <iacopo@icpmol.es>
2024-12-13 18:43:43 +0100

    doesn't exactly work...
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>

--- src/homework_4/homework4pkg/src/feedback_node.cpp
+++ src/homework_4/homework4pkg/src/feedback_node.cpp
@@ -55,10 +55,10 @@ void node::Prepare(void)
 {
 	 RunPeriod = RUN_PERIOD_DEFAULT;
 
-    node_name = ros::this_node::getName();
+  node_name = ros::this_node::getName();
 
-    Handle.getParam("/xr", xr);
-    Handle.getParam("/yr", yr);
+  Handle.getParam("/xr", xr);
+  Handle.getParam("/yr", yr);
     	
 	Handle.getParam(node_name+"/R",R);
 	Handle.getParam(node_name+"/T",T);
@@ -88,6 +88,7 @@ void node::RunPeriodically(float Period)
   ROS_INFO("Node %s: params xr: %f yr: %f R: %f T: %f", ros::this_node::getName().c_str(),xr,yr,R,T);  
 
 	while (ros::ok()) {
+    node::PeriodicTask();
 		ros::spinOnce(); 
 		LoopRate.sleep();
 	}
@@ -150,53 +151,6 @@ void node::tb_MessageCallback(const std_msgs::Float64MultiArray::ConstPtr& msg) 
     xp  = msg->data[4];
     yp  = msg->data[5];
     
-    // double t = ros::Time::now().toSec();
-    // double phi = 2*3.14/T;
-
-    // // xp_dot = derivative for feedroward
-    // xp_dot = - R * phi * sin(phi*t);
-    // yp_dot = R * phi * cos(phi*t);
-    // // xsp_dot = trajectory
-    // double xsp =  R * ( cos(phi*t) - 1);
-    // double ysp = R * sin(phi*t);
-
-    
-
-    // PID
-        // pidx_ysp_act = xsp;
-        // pidy_ysp_act = ysp;
-
-        // pidx_y_act = xp;
-        // pidy_y_act = yp;
-        // double ex = (pidx_ysp_act-pidx_y_act);
-        // double ey = (pidy_ysp_act-pidy_y_act);
-
-        // double pidx_uI_act = pidx_uI_prev+pid_a*(pidx_ysp_act-pidx_y_act);
-        // double pidx_uP_act = pid_b*(pidx_ysp_act-pidx_y_act);
-        // pidx_u_act = pidx_uP_act+pidx_uI_act+xp_dot;
-        // pidx_uI_prev = pidx_uI_act;
-
-        // double pidy_uI_act = pidy_uI_prev+pid_a*(pidy_ysp_act-pidy_y_act);
-        // double pidy_uP_act = pid_b*(pidy_ysp_act-pidy_y_act);
-        // pidy_u_act = pidy_uP_act+pidy_uI_act+yp_dot; //proportional + integrator + feedforward
-        // pidy_uI_prev = pidy_uI_act;    
-
-    // linearization
-
-    // double v, omega;
-
-    // v = (cos(theta)-yr*sin(theta)/xr)*pidx_u_act+(sin(theta)+yr*cos(theta)/xr)*pidy_u_act;
-    // omega = (- sin(theta)*pidx_u_act + cos(theta)*pidy_u_act)/xr;
-
-    // // publishing
-    // std_msgs::Float64MultiArray msgtosend;
-    // msgtosend.data.resize(3);
-    // msgtosend.data[0] = ros::Time::now().toSec();
-    // msgtosend.data[1] = v;
-    // msgtosend.data[2] = omega;
-
-    // // ROS_INFO("Node %s: received theta: %f\n calculated v:%f w: %f", ros::this_node::getName().c_str(),theta,v,omega);
-    // publisher.publish(msgtosend);
 }
 
 
